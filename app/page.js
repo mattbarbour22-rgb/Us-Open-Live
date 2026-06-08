eea6813d689bf4fd5878527827cc6c67b89af5f2
mattbarbour22-rgb <mattbarbour22@gmail.com>
2026-06-08 21:24:18 +1200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -679,7 +679,7 @@ const eliminatedCount = pool.filter(p => p.eliminated).length;
         </div>
       </div>
 
-      <div className="note">Live rankings are decided by best current golf position, then next best and third pick. Final results are preliminary and must be verified by Jonesy. </div>
+      <div className="note">Live rankings are decided by best current golf position, then next best and third pick. To win, your picks must include the outright U.S. Open winner. Final results are preliminary and must be verified by Jonesy. </div>
     </main>
   );
 }
